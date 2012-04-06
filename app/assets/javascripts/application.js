7e622292178686d80159d5a6533616bef51e0456
Andrea D'Amico <andrea.damico@gmail.com>
2012-04-06 16:55:21 +0200

    adding jquery maskedinput for arrival/departure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require jquery.maskedinput
 //= require common
 //= require_tree .
--- app/assets/javascripts/common.js.coffee
+++ app/assets/javascripts/common.js.coffee
@@ -1,1 +1,5 @@
 $(".nav-tabs a:first").tab('show')
+
+for field in ["arrival", "departure"]
+  $("#registration_#{field}").mask("99/99/9999")
+
